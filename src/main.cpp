1e23a4e0d5cf723e81b7516b28b6d34649994d5d
Nick Currier <nicholas.currier@gmail.com>
2015-03-08 20:39:33 -0700

    Changes to gitignores and build files.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+  std::cout << "*******************************" << std::endl;
+  std::cout << "*** Welcome to Proteus 6DOF ***" << std::endl;
+  std::cout << "*******************************" << std::endl;
+
+  return 0;
+}
